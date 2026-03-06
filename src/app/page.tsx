0f7f403e10805c27a82d860779f9bbc59d5f2f83
Adithya Sunil.A <adithyasunila2003@gmail.com>
2026-03-06 10:46:32 +0530

    task compeleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Base from "@/components/Base";
-import Content from "@/components/Content";
+import Base from "@/src/components/Base";
+import Content from "@/src/components/Content";
 
 export default function Home() {
   return (
